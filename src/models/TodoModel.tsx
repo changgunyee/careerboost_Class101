d3aee97b51587f5783dcfbd70894b2c72cc6c6c8
changgunyee <minyoung403@naver.com>
2019-09-23 22:59:46 +0900

    Refactor: Change STATES to string and set id start from 0

--- src/models/TodoModel.tsx
+++ src/models/TodoModel.tsx
@@ -1,17 +1,18 @@
 import {observable} from "mobx";
 
 export enum STATES{
-    TODO,DONE
+    TODO='TODO',
+    DONE='DONE'
 }
 
 export class TodoModel{
     readonly id:number;
     @observable private _content:string;
-    @observable private _state:number;
+    @observable private _state:STATES;
     @observable private _isBookMarked:boolean;
-    private static _newId:number=1;
+    private static _newId:number=0;
 
-    constructor(content:string, state:number=STATES.TODO,isBookMarked:boolean=false){
+    constructor(content:string, state:STATES=STATES.TODO,isBookMarked:boolean=false){
         this.id=TodoModel._newId++;
         this._content=content;
         this._state=state;
@@ -27,11 +28,11 @@ export class TodoModel{
         this._content = value;
     }
 
-    get state(): number {
+    get state(): STATES {
         return this._state;
     }
 
-    set state(value: number) {
+    set state(value: STATES) {
         this._state = value;
     }
 
